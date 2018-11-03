cc524f046766e05bf6304270c528179adac9df6d
rei-m <star.oceans.rm@gmail.com>
2018-11-03 17:03:10 +0900

    initialize

---
+++ app/src/main/java/me/reim/androidtemplate/App.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2018. Rei Matsushita
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+ * the License for the specific language governing permissions and limitations under the License.
+ */
+
+package me.reim.androidtemplate
+
+import android.content.Context
+import androidx.multidex.MultiDex
+import com.jakewharton.threetenabp.AndroidThreeTen
+import com.squareup.leakcanary.LeakCanary
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+import dagger.android.support.DaggerApplication
+import timber.log.Timber
+import javax.inject.Singleton
+
+open class App : DaggerApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        initLeakCanary()
+        initTimber()
+        initThreeTenABP()
+    }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerApp_Component.builder()
+//            .application(this)
+//            .networkModule(NetworkModule.instance)
+//            .databaseModule(DatabaseModule.instance)
+            .build()
+    }
+
+    protected open fun initLeakCanary() {
+        if (BuildConfig.DEBUG) {
+            LeakCanary.install(this)
+        }
+    }
+
+    protected open fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+//            Timber.plant(CrashlyticsTree())
+        }
+    }
+
+    private fun initThreeTenABP() {
+        AndroidThreeTen.init(this)
+
+    }
+
+    @Singleton
+    @dagger.Component(
+        modules = [
+            AndroidSupportInjectionModule::class
+        ]
+    )
+    interface Component : AndroidInjector<App>
+}
